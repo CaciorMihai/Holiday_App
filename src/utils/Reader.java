c41beb03048d6f038db83761193d02fb851450f0
mihai.cacior@stud.acs.upb.ro <mihaicacior@yahoo.com>
2018-03-05 18:32:18 +0200

    made intellij project and readme

---
+++ src/utils/Reader.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Reader {
+    private static Reader ourInstance = new Reader();
+
+    public static Reader getInstance() {
+        return ourInstance;
+    }
+
+    private Reader() {
+    }
+
+}
