f5e877737318e01aef83cfee8186d0dd9d173142
mihai.cacior@stud.acs.upb.ro <mihaicacior@yahoo.com>
2018-03-05 23:28:37 +0200

    test file inputs

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,8 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-//        dateManagerTest();
-//        readerTest();
+        dateManagerTest();
+        readerTest();
         hierarchyTest("./test_input_1");
         hierarchyTest("./test_input_2");
     }
